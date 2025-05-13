e652e715d9e2ef4788110180a8e30eaedb22e96b
Qendresa Hoti <qendresahhoti@gmail.com>
2025-05-13 23:43:19 +0200

    Refactor

--- src/app/components/PromptTreeTooltip.tsx
+++ src/app/components/PromptTreeTooltip.tsx
@@ -1,9 +1,22 @@
 "use client";
 
 import React from "react";
-import ReactFlow, { Background } from "reactflow";
+import ReactFlow, { Background, ReactFlowProps } from "reactflow";
 import { NODE_TYPES, INITIAL_NODES, INITIAL_EDGES } from "./flowNodes";
 import "reactflow/dist/style.css";
+
+// Flow configuration
+const flowConfig: Partial<ReactFlowProps> = {
+  fitView: true,
+  minZoom: 0.5,
+  maxZoom: 1,
+  nodesDraggable: false,
+  nodesConnectable: false,
+  elementsSelectable: false,
+  panOnDrag: false,
+  zoomOnScroll: false,
+  proOptions: { hideAttribution: true },
+};
 
 export default function PromptTreeTooltip() {
   return (
@@ -12,15 +25,7 @@ export default function PromptTreeTooltip() {
         nodes={INITIAL_NODES}
         edges={INITIAL_EDGES}
         nodeTypes={NODE_TYPES}
-        fitView
-        minZoom={0.5}
-        maxZoom={1}
-        nodesDraggable={false}
-        nodesConnectable={false}
-        elementsSelectable={false}
-        panOnDrag={false}
-        zoomOnScroll={false}
-        proOptions={{ hideAttribution: true }}
+        {...flowConfig}
       >
         <Background color="#f1f5f9" gap={16} />
       </ReactFlow>
