8b2194aab0028b6d78e8bb5769f44cccda1a6380
Qendresa Hoti <qendresahhoti@gmail.com>
2025-05-16 23:47:25 +0200

    remove unused code

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-type HeaderProps = {
-  onAddClick?: () => void;
-};
-
-export default function Header({ onAddClick }: HeaderProps) {
+export default function Header() {
   return (
     <header className="sticky top-0 z-10 bg-white">
       <div className="flex items-center justify-between px-4 py-2">
